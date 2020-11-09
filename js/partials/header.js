9198970b9d76ee9eb242988aea5678d6c7c6e599
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-09 11:14:07 +0100

    Vattenkruka initial setup

---
+++ js/partials/header.js
@@ -0,0 +1,15 @@
+document.getElementById("header").innerHTML = `
+    <header>
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html">Home</a>
+        
+            <div class="collapse navbar-collapse" id="navbarText">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Reddit <span class="sr-only">(current)</span></a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+`
