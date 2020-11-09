d66b43c6ddce540725abd2d378136be09bc7c2b6
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-09 15:40:18 +0100

    Vattenkruka & lillap feat: createCard function added

--- js/scripts/app.js
+++ js/scripts/app.js
@@ -2,22 +2,59 @@ const threadTitles = [];
 const threadUrlList = [];
 const authors = [];
 
+
+const contentElement = document.querySelector("#content");
+
 $(document).ready(function () {
     console.log("Document loaded");
-    $.ajax({
+ $.ajax({
         url: "https://www.reddit.com/r/choosingBeggars/top.json?limit=10",
         method: "GET",
         success: function (response) {
-            console.log();
-            for(let i = 0 ; i <= response.data.children.length ; i++){
+            console.log("Get method was successful");
+            for (let i = 0; i <= response.data.children.length; i++) {
                 threadTitles.push(response.data.children[i].data.title);
                 threadUrlList.push(response.data.children[i].data.permalink);
                 authors.push(response.data.children[i].data.author);
-                
+               
+                createCard(threadTitles[i], threadUrlList[i], authors[i]);
             }
         },
         error: function () {
             console.log("Error: Unable to fetch titles.");
         },
+      
+
+    }).done(function (response){
+        console.log('done()', response);
     });
+   
 });
+
+//<div id="postTitle"></div>
+//<div id="postUrl"></div>
+//author
+
+function createCard(threadTitle, threadUrl, author){
+    
+        
+     const card = document.createElement('div');
+        contentElement.appendChild(card);
+    card.innerHTML = `
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+    <h5 class="card-title">${threadTitle}</h5>
+    <p class="card-text">${author}</p>
+    <a href="${threadUrl}" class="btn btn-primary">Take me to the post</a>
+  </div>
+</div>
+    `
+    
+    
+}
+
+
+
+
+
+
