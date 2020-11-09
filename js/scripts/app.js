8859e822f715d27c95f048324ef0c7e89c8c2e55
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-09 14:18:45 +0100

    Vattenkruka fix: added lists for titles and permalinks for the get method

--- js/scripts/app.js
+++ js/scripts/app.js
@@ -1,11 +1,17 @@
+const threadTitles = [];
+const threadUrlList = [];
+
 $(document).ready(function () {
     console.log("Document loaded");
     $.ajax({
         url: "https://www.reddit.com/r/choosingBeggars/top.json?limit=10",
         method: "GET",
         success: function (response) {
-            console.log(response);
-            $("#content").html(response.title);
+            console.log();
+            for(let i = 0 ; i <= response.data.children.length ; i++){
+                threadTitles.push(response.data.children[i].data.title);
+                threadUrlList.push(response.data.children[i].data.permalink);
+            }
         },
         error: function () {
             console.log("Error: Unable to fetch titles.");
